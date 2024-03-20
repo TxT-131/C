f6d386820a5b2a2772ce142b9b54c5076311503a
TxT-131 <152673659+TxT-131@users.noreply.github.com>
2024-03-20 23:35:43 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,176 @@
+import turtle as t
+# t.left(90)
+# t.circle(100)
+# t.left(180)
+# t.circle(100)
+# t.right(90)
+# t.circle(100)
+# t.right(180)
+# t.circle(100)
+
+
+
+# t.speed(10)
+# t.color('red')
+# for i in range(12):
+#     t.right(91)
+#     t.circle(100)
+
+
+
+#五环
+# t.pensize(10)
+#
+# t.color('black')
+# t.circle(75)
+#
+# t.penup()
+# t.goto(-160,0)
+# t.pendown()
+# t.color('blue')
+# t.circle(75)
+#
+# t.penup()
+# t.goto(160,0)
+# t.pendown()
+# t.color('red')
+# t.circle(75)
+#
+# t.penup()
+# t.goto(80,-100)
+# t.pendown()
+# t.color('green')
+# t.circle(75)
+#
+# t.penup()
+# t.goto(-80,-100)
+# t.pendown()
+# t.color('yellow')
+# t.circle(75)
+#
+# t.color('black')
+# t.penup()
+# t.goto(-100,180)
+# t.pendown()
+# t.write('北京 2024',font=('YouYuan',32))
+#
+# input("Press any key to exit..."),
+
+
+
+#美盾
+# '''t.penup()
+# t.goto(0,-200)
+# t.pendown()
+# t.color('red')
+# t.begin_fill()
+# t.circle(200)
+# t.end_fill()
+#
+# t.penup()
+# t.goto(0,-150)
+# t.pendown()
+# t.color('white')
+# t.begin_fill()
+# t.circle(150)
+# t.end_fill()
+#
+# t.penup()
+# t.goto(0,-100)
+# t.pendown()
+# t.color('red')
+# t.begin_fill()
+# t.circle(100)
+# t.end_fill()
+#
+# t.penup()
+# t.goto(0,-50)
+# t.pendown()
+# t.color('blue')
+# t.begin_fill()
+# t.circle(50)
+# t.end_fill()
+#
+# t.penup()
+# t.goto(-40,10)
+# t.color('white')
+# t.begin_fill()
+# t.pendown()
+#
+# for i in range (5):
+#     t.forward(80)
+#     t.right(144)
+# t.end_fill()
+#
+# t.hideturtle()
+
+
+#
+# #随机数
+# import random
+#
+# t.colormode(255)
+# t.speed(0)
+#
+# for i in range(20):
+#
+#     red=random.randint(0,255)
+#     green=random.randint(0,255)
+#     blue=random.randint(0,255)
+#
+#     x=random.randint(-220,220)
+#     y=random.randint(-100,200)
+#
+#     t.penup()
+#     t.goto(x,y)
+#     t.pendown()
+#
+#     t.color(red,green,blue)
+#
+#     t.begin_fill()
+#     t.circle(30)
+#     t.end_fill()
+#
+#     t.right(90)
+#     t.forward(30)
+#     t.left(90)
+#
+
+# t.bgcolor('black')
+# t.pensize(100)
+# t.penup()
+# t.goto(-250,100)
+# t.pendown()
+# t.forward(700)
+#
+# t.color(50,50,50)
+# t.penup()
+# t.goto(-300,200)
+# t.pendown()
+# t.forward(700)
+#
+# t.color(75,75,75)
+# t.penup()
+# t.goto(-350,100)
+# t.pendown()
+# t.forward(700)
+#
+# t.color(100,100,100)
+# t.penup()
+# t.goto(-250,100)
+# t.pendown()
+# t.forward(700)
+#
+# t.color(125,125,125)
+# t.penup()
+# t.goto(-350,200)
+# t.pendown()
+# t.forward(700)
+#
+# t.color(150,150,150)
+# t.penup()
+# t.goto(-350,300)
+# t.pendown()
+# t.forward(700)
+#
+# t.done()
